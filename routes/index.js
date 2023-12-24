ab9ba24ed01859b0f792068cb8cdb53baceec2b5
Yousef Alhindawi <100360492+yousefalhindawi@users.noreply.github.com>
2023-12-24 11:46:30 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Production Environment' });
+  res.render('index', { title: 'Development Environment' });
 });
 
 module.exports = router;
